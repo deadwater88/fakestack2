bb429df5607884cde1d2b2ac4c7edcf90801ab9a
David Wong <deadwater88@gmail.com>
2017-06-24 23:06:52 -0700

    update relevantUsers Controller

--- fakestack/app/controllers/api/relevant_users_controller.rb
+++ fakestack/app/controllers/api/relevant_users_controller.rb
@@ -1,7 +1,7 @@
 class Api::RelevantUsersController < ApplicationController
   def index
     query = params[:user_id].downcase
-    @users = User.all.where("lower(first_name) LIKE ? OR lower(last_name) LIKE ?", "%#{query}%", "%#{query}%").limit(15).order(:last_name)
+    @users = User.where("lower(first_name) LIKE ? OR lower(last_name) LIKE ?", "%#{query}%", "%#{query}%").limit(15).order(:last_name)
     # @users = User.all.includes(:requesters).includes(:recipients)
     render :index
   end
