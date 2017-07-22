75cef76b7722d1ff577ab254c3bca2e155c1f1fe
David Wong <david.wong.cal@gmail.com>
2017-07-22 12:35:41 -0700

    fix conversation validator typo

--- fakestack/app/models/conversation.rb
+++ fakestack/app/models/conversation.rb
@@ -1,5 +1,5 @@
 class Conversation < ApplicationRecord
   validates :participant1_id, :participant2_id, presence: true
-  validates :participant1_id, unqiueness: {scope: :participant2_id}
+  validates :participant1_id, uniqueness: {scope: :participant2_id}
 
 end
