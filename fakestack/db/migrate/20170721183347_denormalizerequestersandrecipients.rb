617efd40fad45a0203d01284e5302582791ace33
David Wong <david.wong.cal@gmail.com>
2017-07-31 07:40:43 -0700

    rewrite migrations to use array: true for default {}

--- fakestack/db/migrate/20170721183347_denormalizerequestersandrecipients.rb
+++ fakestack/db/migrate/20170721183347_denormalizerequestersandrecipients.rb
@@ -1,6 +1,6 @@
 class Denormalizerequestersandrecipients < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :requesters, :text, default: {}
-    add_column :users, :recipients, :text, default: {}
+    add_column :users, :requesters, :text, default: {}, array: true
+    add_column :users, :recipients, :text, default: {}, array: true
   end
 end
