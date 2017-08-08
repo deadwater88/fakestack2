791f459b97592d5a997829f625cce9f1d1f030d3
David Wong <david.wong.cal@gmail.com>
2017-08-08 15:57:07 -0700

    style upload modal

--- fakestack/frontend/components/profile/photos/upload_modal.jsx
+++ fakestack/frontend/components/profile/photos/upload_modal.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import Modal from 'react-modal';
+import values from 'lodash/values';
+import ProfileIcon from '../profile_icon';
+import {FaClose} from 'react-icons/lib/fa/';
 const customStyles = {
   content : {
     top                   : '50%',
@@ -18,7 +21,8 @@ class UploadModal extends React.Component {
 
   constructor(props){
     super(props);
-    this.state = {files: props.files};
+    this.uploadPhotos = this.uploadPhotos.bind(this);
+    this.state = {files: {}};
     this.state.modalprops = {style: customStyles,
                   onAfterOpen: this.onAfterOpen,
                   onRequestClose: props.onRequestClose,
@@ -27,18 +31,42 @@ class UploadModal extends React.Component {
                   Modal.setAppElement('body');
   }
 
-  componentWillReceiveProps(newProps){
-    let files = newProps.files;
+  addFile(file){
+    let files = this.state.files;
+    files[file.name] = file;
     this.setState({files});
-    debugger;
+    const reader = new FileReader();
+    reader.addEventListener("load", ()=> {
+      file.src = reader.result;
+      this.setState({files});
+  }, false);
+    reader.readAsDataURL(file);
+  }
+
+  componentWillReceiveProps(newprops){
+    let filelist = newprops.files;
+    for (let i = 0; i < filelist.length; i++) {
+      this.addFile(filelist[i]);
+    }
   }
 
   onAfterOpen(){
     console.log("opened Modal");
   }
 
+  closeModal(e){
+    e.preventDefault();
+    this.setState({files:{}});
+    this.props.onRequestClose();
+  }
+
+  uploadPhotos(e){
+    e.preventDefault();
+    this.props.uploadPhotos(this.state.files);
+  }
 
   render(){
+    let files = values(this.state.files);
     return (
       <Modal
         {...this.state.modalprops} isOpen={this.props.open}>
@@ -46,15 +74,36 @@ class UploadModal extends React.Component {
           <div className="uploadModalScreen">
 
           </div>
-          <div className="uploadModalContent">
-            UploadModal
-            {this.state.files.map((file)=>{
-              console.log(file);
-              return <div key={file.name}>
-                {file.name}
-              </div>;
-            })}
+
+          <header className="uploadHeader">
+            <h2 className="bold"> Upload Photos/Videos </h2>
+            <FaClose onClick={this.closeModal} className="cancelUploadModal"/>
+          </header>
+
+          <div className="uploadInputForm">
+            <ProfileIcon className="profileIcon"/>
+            <input placeholder="Say something about this photo..." />
           </div>
+
+          <li className="uploadedPhotosContainer">
+            {files.map((file)=>{
+              console.log(file);
+              return <ul key={file.name} className="imageUpload">
+                <img src={file.src}/>
+              </ul>;
+            })}
+          </li>
+
+          <div className="tags">
+
+          </div>
+
+          <div className="uploadbuttonContainer">
+            <button onClick={this.uploadPhotos} className="blue-submit-button">
+              Post
+            </button>
+          </div>
+
         </div>
       </Modal>
     );
